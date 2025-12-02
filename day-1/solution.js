a28b84465a438d5e1f8cfe39db27e1d00634862e
C-M-42 <211462903+C-M-42@users.noreply.github.com>
2025-12-02 14:49:44 +0100

    feat(solution): solve day 1 puzzle (#1)
    * chore: initialize "day"
    
    Add dummy artifacts 'puzzle-input.txt' and 'solution.js'.
    
    * feat: add puzzle input
    
    Replaced the initial dummy 'puzzle-input.txt' artifact with the actual
    puzzle input.
    
    * feat(part-1): find solution
    
    Implemented the logic to compute the solution code for part 1.
    
    * feat(part-2): find solution
    
    Implemented the logic to compute the solution code for part 2.

---
+++ day-1/solution.js
@@ -0,0 +1,64 @@
+function readPuzzleInputAsString(file = 'puzzle-input.txt') {
+  const fs = require('node:fs');
+  try {
+    const data = fs.readFileSync(file, 'utf8');
+    return data;
+  } catch (err) {
+    console.error('FAILED_TO_READ_PUZZLE_INPUT', err);
+  }
+}
+
+function parsePuzzleInput(puzzleInputAsString = readPuzzleInputAsString()) {
+  let puzzleInput = []
+  puzzleInputAsString
+    .split(/\r?\n/)
+    .forEach(line => {
+      if (line.trim()) {
+        puzzleInput.push({
+          direction: line.charAt(0).toUpperCase() === 'L' ? -1 : 1,
+          distance: Number(line.substr(1))
+        });
+      }
+    });
+  return puzzleInput;
+}
+
+function solvePuzzlePart1(puzzle_input = parsePuzzleInput()) {
+  const RING_SIZE = 100;
+
+  let originStopCount = 0;
+
+  let position = 50;
+  puzzle_input.forEach(({direction, distance}) => {
+    position += distance * direction;
+    position = ((position % RING_SIZE) + RING_SIZE) % RING_SIZE;
+
+    if (position === 0 || position === RING_SIZE) {
+      position = 0;
+      ++originStopCount;
+    }
+  });
+
+  return originStopCount;
+}
+
+function solvePuzzlePart2(puzzle_input = parsePuzzleInput()) {
+  const RING_SIZE = 100;
+
+  let originPassCount = 0;
+
+  let position = 50;
+  puzzle_input.forEach(({direction, distance}) => {
+    for (let i = 0; i < distance; ++i) {
+      position = (position + direction + RING_SIZE) % RING_SIZE;
+      if (position === 0) {
+        ++originPassCount;
+      }
+    }
+  });
+
+  return originPassCount;
+}
+
+console.log('SOLUTION::PART-1', solvePuzzlePart1());
+console.log('SOLUTION::PART-2', solvePuzzlePart2());
