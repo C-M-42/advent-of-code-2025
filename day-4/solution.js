803007a13f874ebdd7efc88db3f4aacf60b17425
C-M-42 <211462903+C-M-42@users.noreply.github.com>
2025-12-05 14:04:35 +0100

    feat(part-1): find solution
    Implemented the logic to compute the solution code for part 1.

--- day-4/solution.js
+++ day-4/solution.js
@@ -11,21 +11,73 @@ function readPuzzleInputAsString(file = 'puzzle-input.txt') {
 }
 
 function parsePuzzleInput(puzzleInputAsString = readPuzzleInputAsString()) {
-    let puzzleInput = []
+    const puzzleInput = [];
     puzzleInputAsString
         .trim()
         .split(/\r?\n/)
         .forEach(line => {
             puzzleInput.push(
                 line.split('')
-                    .map(digit => Number(digit))
             );
         });
     return puzzleInput;
 }
 
 function solvePuzzlePart1(puzzle_input = parsePuzzleInput()) {
-    // TODO: Implement solution for Step #1.
+    const lastPosition = {
+        x: puzzle_input[0].length - 1,
+        y: puzzle_input.length - 1
+    };
+
+    const isPositionFree = (x, y) => {
+        return puzzle_input[y][x] !== '@' && puzzle_input[y][x] === '.';
+    };
+
+    const isPositionAccessible = (x, y) => {
+        const surroundingPositionCandidates = [
+            {x: x - 1, y: y - 1},
+            {x: x - 1, y: y},
+            {x: x - 1, y: y + 1},
+
+            {x: x + 1, y: y - 1},
+            {x: x + 1, y: y},
+            {x: x + 1, y: y + 1},
+
+            {x: x, y: y - 1},
+            {x: x, y: y + 1}
+        ];
+
+        let nonAccessiblePositionsCount = 0;
+        surroundingPositionCandidates
+            .filter((candidate) => {
+                return (
+                        candidate.x < 0
+                        || candidate.y < 0
+                        || candidate.y > lastPosition.y
+                        || candidate.x > lastPosition.x
+                    ) !== true;
+            })
+            .filter((position) => {
+                return !isPositionFree(position.x, position.y);
+            })
+            .forEach(() => {
+                ++nonAccessiblePositionsCount;
+            });
+
+        return nonAccessiblePositionsCount < 4;
+    };
+
+    let result = 0;
+    for (let row = lastPosition.y; row >= 0; --row) {
+        for (let column = lastPosition.x; column >= 0; --column) {
+            if (!isPositionFree(column, row)
+                && isPositionAccessible(column, row)) {
+                ++result;
+            }
+        }
+    }
+
+    return result;
 }
 
 function solvePuzzlePart2(puzzle_input = parsePuzzleInput()) {
