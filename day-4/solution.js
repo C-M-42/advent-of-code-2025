a8701b5a7a0aa164d4f143f4e391ce12735d2b17
C-M-42 <211462903+C-M-42@users.noreply.github.com>
2025-12-08 11:51:08 +0100

    feat(solution): solve day 4 puzzle (#4)
    * chore: initialize "day"
    
    Add dummy artifacts 'puzzle-input.txt' and 'solution.js'.
    
    * feat: add puzzle input
    
    Replaced the initial dummy 'puzzle-input.txt' artifact with the actual
    puzzle input.
    
    * feat(part-1): find solution
    
    Implemented the logic to compute the solution code for part 1.
    
    * feat(part-2): find solution
    
    Implemented the logic to compute the solution code for part 2.

---
+++ day-4/solution.js
@@ -0,0 +1,148 @@
+// noinspection JSUnresolvedReference
+// noinspection DuplicatedCode
+
+function readPuzzleInputAsString(file = 'puzzle-input.txt') {
+    const fs = require('node:fs');
+    try {
+        return fs.readFileSync(file, 'utf8');
+    } catch (err) {
+        console.error('FAILED_TO_READ_PUZZLE_INPUT', err);
+    }
+}
+
+function parsePuzzleInput(puzzleInputAsString = readPuzzleInputAsString()) {
+    const puzzleInput = [];
+    puzzleInputAsString
+        .trim()
+        .split(/\r?\n/)
+        .forEach(line => {
+            puzzleInput.push(
+                line.split('')
+            );
+        });
+    return puzzleInput;
+}
+
+function solvePuzzlePart1(puzzle_input = parsePuzzleInput()) {
+    const lastPosition = {
+        x: puzzle_input[0].length - 1,
+        y: puzzle_input.length - 1
+    };
+
+    const isPositionFree = (x, y) => {
+        return puzzle_input[y][x] !== '@' && puzzle_input[y][x] === '.';
+    };
+
+    const isPositionAccessible = (x, y) => {
+        const surroundingPositionCandidates = [
+            {x: x - 1, y: y - 1},
+            {x: x - 1, y: y},
+            {x: x - 1, y: y + 1},
+
+            {x: x + 1, y: y - 1},
+            {x: x + 1, y: y},
+            {x: x + 1, y: y + 1},
+
+            {x: x, y: y - 1},
+            {x: x, y: y + 1}
+        ];
+
+        let nonAccessiblePositionsCount = 0;
+        surroundingPositionCandidates
+            .filter((candidate) => {
+                return (
+                        candidate.x < 0
+                        || candidate.y < 0
+                        || candidate.y > lastPosition.y
+                        || candidate.x > lastPosition.x
+                    ) !== true;
+            })
+            .filter((position) => {
+                return !isPositionFree(position.x, position.y);
+            })
+            .forEach(() => {
+                ++nonAccessiblePositionsCount;
+            });
+
+        return nonAccessiblePositionsCount < 4;
+    };
+
+    let result = 0;
+    for (let row = lastPosition.y; row >= 0; --row) {
+        for (let column = lastPosition.x; column >= 0; --column) {
+            if (!isPositionFree(column, row)
+                && isPositionAccessible(column, row)) {
+                ++result;
+            }
+        }
+    }
+
+    return result;
+}
+
+function solvePuzzlePart2(puzzle_input = parsePuzzleInput()) {
+    const lastPosition = {
+        x: puzzle_input[0].length - 1,
+        y: puzzle_input.length - 1
+    };
+
+    const isPositionFree = (x, y) => {
+        return puzzle_input[y][x] !== '@' && puzzle_input[y][x] === '.';
+    };
+
+    const isPositionAccessible = (x, y) => {
+        const surroundingPositionCandidates = [
+            {x: x - 1, y: y - 1},
+            {x: x - 1, y: y},
+            {x: x - 1, y: y + 1},
+
+            {x: x + 1, y: y - 1},
+            {x: x + 1, y: y},
+            {x: x + 1, y: y + 1},
+
+            {x: x, y: y - 1},
+            {x: x, y: y + 1}
+        ];
+
+        let nonAccessiblePositionsCount = 0;
+        surroundingPositionCandidates
+            .filter((candidate) => {
+                return (
+                    candidate.x < 0
+                    || candidate.y < 0
+                    || candidate.y > lastPosition.y
+                    || candidate.x > lastPosition.x
+                ) !== true;
+            })
+            .filter((position) => {
+                return !isPositionFree(position.x, position.y);
+            })
+            .forEach(() => {
+                ++nonAccessiblePositionsCount;
+            });
+
+        return nonAccessiblePositionsCount < 4;
+    };
+
+    let result = 0;
+    let rollsRemoved;
+    do {
+        rollsRemoved = 0;
+
+        for (let row = 0; row <= lastPosition.y; ++row) {
+            for (let column = 0; column <= lastPosition.x; ++column) {
+                if (!isPositionFree(column, row)
+                    && isPositionAccessible(column, row)) {
+                    ++rollsRemoved;
+                    puzzle_input[row][column] = '.';
+                    ++result;
+                }
+            }
+        }
+    } while (rollsRemoved > 0);
+
+    return result;
+}
+
+console.log('SOLUTION::PART-1', solvePuzzlePart1());
+console.log('SOLUTION::PART-2', solvePuzzlePart2());
