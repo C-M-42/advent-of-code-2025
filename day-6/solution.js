69e38e26d5eaf305988bdeac9a3ae93a6eef8eb6
C-M-42 <211462903+C-M-42@users.noreply.github.com>
2025-12-08 14:55:05 +0100

    chore: initialize "day"
    Add dummy artifacts 'puzzle-input.txt' and 'solution.js'.

---
+++ day-6/solution.js
@@ -0,0 +1,48 @@
+// noinspection JSUnresolvedReference
+// noinspection DuplicatedCode
+
+function readPuzzleInputAsString(file = 'puzzle-input.txt') {
+    const fs = require('node:fs');
+    try {
+        return fs.readFileSync(file, 'utf8');
+    } catch (err) {
+        console.error('FAILED_TO_READ_PUZZLE_INPUT', err);
+    }
+}
+
+function parsePuzzleInput(puzzleInputAsString = readPuzzleInputAsString()) {
+    const puzzleInput = {
+        freshRanges: [],
+        ids: []
+    };
+
+    let isParsingRanges = true;
+    puzzleInputAsString
+        .trim()
+        .split(/\r?\n/)
+        .forEach(line => {
+            if (line.trim() === '') {
+                isParsingRanges = false;
+            } else if (isParsingRanges) {
+                const rangeBoundaries = line.trim().split('-');
+                puzzleInput.freshRanges.push({
+                    min: Number(rangeBoundaries[0]),
+                    max: Number(rangeBoundaries[1])
+                });
+            } else {
+                puzzleInput.ids.push(Number(line.trim()));
+            }
+        });
+    return puzzleInput;
+}
+
+function solvePuzzlePart1(puzzleInput = parsePuzzleInput()) {
+    // TODO: Implement solution for Step #1.
+}
+
+function solvePuzzlePart2(puzzleInput = parsePuzzleInput()) {
+    // TODO: Implement solution for Step #2.
+}
+
+console.log('SOLUTION::PART-1', solvePuzzlePart1());
+console.log('SOLUTION::PART-2', solvePuzzlePart2());
